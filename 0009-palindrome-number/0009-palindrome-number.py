904e4586914ac69ae35ee055ecb46e01c45252e2
divya-3547 <94342358+divya-3547@users.noreply.github.com>
2026-08-08 15:10:31 +0530

    Time: 9 ms (63.7%), Space: 12.5 MB (18.03%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        original = x
+        reversed_num = 0
+        while x > 0:
+            reversed_num = reversed_num * 10 + x % 10
+            x //= 10
+        return original == reversed_num
